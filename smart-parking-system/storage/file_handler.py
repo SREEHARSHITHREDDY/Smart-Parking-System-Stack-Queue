c43f2a06ca263531df9fe7cdb6cd28d61c2e11c7
SREEHARSHITH REDDY C <reddyharshith20@gmail.com>
2026-03-04 15:13:14 +0530

    feat: implement persistent storage for parking layout and vehicles

--- smart-parking-system/storage/file_handler.py
+++ smart-parking-system/storage/file_handler.py
@@ -1,22 +1,47 @@
 import json
 import os
+from models.vehicle import Vehicle
 
 DATA_FILE = "data/parking_data.json"
 
 
 def save_data(parking_lot):
+    """
+    Save parking lot state to JSON file
+    """
+
     os.makedirs("data", exist_ok=True)
 
+    data = {
+        "rows": parking_lot.rows,
+        "cols": parking_lot.cols,
+        "revenue": parking_lot.revenue,
+        "layout": {},
+    }
+
+    for slot, info in parking_lot.layout.items():
+
+        if info["vehicle"]:
+            vehicle_data = info["vehicle"].to_dict()
+        else:
+            vehicle_data = None
+
+        data["layout"][slot] = {
+            "status": info["status"],
+            "vehicle": vehicle_data
+        }
+
     with open(DATA_FILE, "w") as f:
-        json.dump({
-            "rows": parking_lot.rows,
-            "cols": parking_lot.cols,
-            "revenue": parking_lot.revenue
-        }, f, indent=4)
+        json.dump(data, f, indent=4)
 
 
 def load_data():
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE) as f:
-            return json.load(f)
-    return None
+    """
+    Load parking lot state from JSON file
+    """
+
+    if not os.path.exists(DATA_FILE):
+        return None
+
+    with open(DATA_FILE) as f:
+        return json.load(f)
