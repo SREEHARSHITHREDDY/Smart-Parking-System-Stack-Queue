0d1e54d922ea3022ccefdc740303ded597db1e97
SREEHARSHITH REDDY C <reddyharshith20@gmail.com>
2026-03-10 08:25:16 +0530

    feat(core): add vehicle number plate validator with Indian format regex

--- smart-parking-system/core/utils.py
+++ smart-parking-system/core/utils.py
@@ -2,7 +2,6 @@ import re
 
 
 def validate_vehicle_number(number_plate):
-
+    # Indian format: TS09AB1234
     pattern = r"^[A-Z]{2}[0-9]{2}[A-Z]{2}[0-9]{4}$"
-
     return bool(re.match(pattern, number_plate))
